c52d0ef7ce7c40e2785f4c1c29ef1714f9bbbb2b
Abhiprayah Tiwari <abhipray@codeaurora.org>
2021-02-01 01:44:54 +0530

    sepolicy: Removed deprecated BOARD_PLAT_* usage
    Removing the use of BOARD_PLAT_* variables as they have
    been deprecated in favor of SYSTEM_EXT_* variables.
    
    Change-Id: I5cb504ef8aaec229c1d88f391af454cf05949480

--- SEPolicy.mk
+++ SEPolicy.mk
@@ -1,20 +1,14 @@
 # Board specific SELinux policy variable definitions
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 SEPOLICY_PATH:= device/qcom/sepolicy
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
-    $(SEPOLICY_PATH)/generic/public
-
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
-    $(SEPOLICY_PATH)/generic/private
-
-BOARD_PLAT_PUBLIC_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PUBLIC_SEPOLICY_DIR) \
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS := \
+    $(SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS) \
+    $(SEPOLICY_PATH)/generic/public \
     $(SEPOLICY_PATH)/qva/public
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := \
-    $(BOARD_PLAT_PRIVATE_SEPOLICY_DIR) \
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS := \
+    $(SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS) \
+    $(SEPOLICY_PATH)/generic/private \
     $(SEPOLICY_PATH)/qva/private
 
 #once all the services are moved to Product /ODM above lines will be removed.
